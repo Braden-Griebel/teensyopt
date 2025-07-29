c35d786b1414c6b4f61f608321ff264ef7aae071
Braden Griebel <bgriebel@uw.edu>
2025-07-28 17:04:26 -0700

    Added arithmetic and boolean comparison operators for Matrix
    Added various arithmetic operator overloads for Matrix class, including
    functions for binary elementwise application of functions. Additionally,
    added some comparison functions, and functions for helping determine if
    any or all of the values in a Matrix are truthy.

--- include/TeensyOpt/TeensyMat/matrix_core.hpp
+++ include/TeensyOpt/TeensyMat/matrix_core.hpp
@@ -1,5 +1,6 @@
 #pragma once
 // std includes
+#include <functional>
 #include <initializer_list>
 #include <stdexcept>
 #include <utility>
@@ -23,6 +24,7 @@ private:
   size_t matrix_size;
 
 public:
+  // SECTION: Constructors
   /*! Construct an empty Matrix with no rows or columns
    * */
   Matrix()
@@ -66,6 +68,52 @@ public:
       throw std::range_error("Received too few elements to initialize Matrix");
     }
   }
+  /*! Construct a Matrix with values from a vector.
+   *
+   * @param nrows Number of rows the new Matrix will have
+   * @param ncols Number of columns the new Matrix will have
+   * @param elements Values of the elements of the new Matrix, will be set in
+   * row major order. Note, the vector will NOT be copied, so
+   * changing its values will alter elements of the Matrix
+   * */
+  Matrix(size_t nrows, size_t ncols, std::vector<Scalar> elements)
+      : data(elements), row_stride(ncols), col_stride(1), nrows(nrows),
+        ncols(ncols), matrix_size(nrows * ncols) {}
+  /*! Construct a Matrix with values from a vector.
+   *
+   * @param nrows Number of rows the new Matrix will have
+   * @param ncols Number of columns the new Matrix will have
+   * @param row_stride The stride for rows in the Matrix (length of step in the
+   * underlying data vector which will increase the row index by 1)
+   * @param col_stride THe stride for columns in the Matrix (length of step
+   * in the underlying data vector which will increase the column index by 1)
+   * @param elements Values of the elements of the new Matrix, will be set in
+   * row major order. Note, the vector will NOT be copied, so
+   * changing its values will alter elements of the Matrix
+   * */
+  Matrix(size_t nrows, size_t ncols, size_t row_stride, size_t col_stride,
+         std::vector<Scalar> elements)
+      : data(elements), row_stride(row_stride), col_stride(col_stride),
+        nrows(nrows), ncols(ncols), matrix_size(nrows * ncols) {}
+
+  // SECTION: Getters
+  /*! Get the number of rows in the Matrix.*/
+  size_t get_nrows() { return this->nrows; }
+  /*! Get the number of columns in the Matrix.*/
+  size_t get_ncols() { return this->ncols; }
+  /*! Get the stride for the rows of the matrix.*/
+  size_t get_row_stride() { return this->row_stride; }
+  /*! Get the stride for the columns of the matrix. */
+  size_t get_col_stride() { return this->col_stride; }
+  /*! Get the underlying data vector*/
+  std::vector<Scalar> *get_data() { return &(this->data); }
+  /*! Get the size of the matrix (the total number of elements).*/
+  size_t get_size() { return this->matrix_size; }
+  /*! Get the shape of the matrix (nrows, ncols). */
+  std::pair<size_t, size_t> get_shape() {
+    return std::pair<size_t, size_t>{this->nrows, this->ncols};
+  }
+  // SECTION: Elementary operations
   /*! Access an element of the matrix by position.
    *
    * @param row Row of the element to be accessed
@@ -137,7 +185,7 @@ public:
    * @param by What to divide the column by
    * */
   void div_col_scalar(size_t column, Scalar by) {
-    this->mult_row_scalar(column, ((Scalar)1) / by);
+    this->mult_col_scalar(column, ((Scalar)1) / by);
   }
   /*! Add a value to each element in a row.
    * @param row Which row to add the value to
@@ -163,7 +211,7 @@ public:
    * @param what What to subtract from the elements of the row
    * */
   void sub_row_scalar(size_t row, Scalar what) {
-    this->sub_row_scalar(row, -what);
+    this->add_row_scalar(row, -what);
   }
   /*! Subtract a value from each element in a column.
    *
@@ -171,7 +219,7 @@ public:
    * @param what What to subtract from the elements of the column
    * */
   void sub_col_scalar(size_t column, Scalar what) {
-    this->sub_row_scalar(column, -what);
+    this->add_col_scalar(column, -what);
   }
   /*! Add row2 to row1, storing the result in row1.
    *
@@ -212,6 +260,266 @@ public:
     for (size_t row = 0; row < this->nrows; row++) {
       *(*this)(row, col1) -= *(*this)(row, col2);
     }
+  }
+  /*! Return the transpose of the matrix*/
+  Matrix<Scalar> transpose() {
+    std::vector<Scalar> transpose_data = this->data;
+    return Matrix<Scalar>{this->ncols, this->nrows, this->col_stride,
+                          this->row_stride, transpose_data};
+  }
+  // SECTION: Operator overloads
+  /*! Apply a binary operator elementwise between this Matrix and Scalar other
+   *
+   * @param other The right hand size Scalar of the function being applied
+   * @param to_apply Binary function to apply to each element of this, and
+   * the Scalar other. Must accept two Scalars and return a single element of
+   * specified Result type.
+   *
+   * @return New Matrix whose elements are the result of the function
+   * */
+  template <typename Result>
+  Matrix<Result>
+  scalar_binary_apply(Scalar other,
+                      std::function<Result(Scalar, Scalar)> to_apply) {
+    Matrix<Result> result_matrix{this->nrows, this->ncols};
+    for (size_t row = 0; row < this->nrows; row++) {
+      for (size_t col = 0; col < this->ncols; col++) {
+        *result_matrix(row, col) = to_apply(*(*this)(row, col), other);
+      }
+    }
+    return result_matrix;
+  }
+  /*! Apply a binary function in place elementwise between this Matrix and
+   * Scalar other
+   *
+   * @param other The right hand size Scalar of the function being applied
+   * @param to_apply Binary function to apply to each element of this, and
+   * the Scalar other
+   *
+   * @return New Matrix whose elements are the result of the function
+   * */
+  void
+  scalar_binary_apply_inplace(Scalar other,
+                              std::function<Scalar(Scalar, Scalar)> to_apply) {
+    for (size_t row = 0; row < this->nrows; row++) {
+      for (size_t col = 0; col < this->ncols; col++) {
+        *(*this)(row, col) = to_apply(*(*this)(row, col), other);
+      }
+    }
+  }
+  /*! Apply a binary function elementwise to two Matrices.
+   *
+   * @param other The right hand side Matrix of the function being applied
+   * @param to_apply Binary function to apply elementwise to this and other,
+   * which accepts two Scalar values and returns a Scalar value
+   *
+   * @return New Matrix whose elements are the result of the function
+   * */
+  template <typename Result>
+  Matrix<Result>
+  elementwise_binary_apply(Matrix<Scalar> const &other,
+                           std::function<Result(Scalar, Scalar)> to_apply) {
+    if ((this->nrows != other.nrows) || (this->ncols != other.ncols)) {
+      throw std::runtime_error("Tried to add Matrices of different shapes");
+    }
+    Matrix<Result> result_matrix{this->nrows, this->ncols};
+    for (size_t row = 0; row < this->nrows; row++) {
+      for (size_t col = 0; col < this->ncols; col++) {
+        *result_matrix(row, col) =
+            to_apply(*(*this)(row, col), *(*this)(row, col));
+      }
+    }
+    return result_matrix;
+  }
+  /*! Apply a binary function in place elementwise to two Matrices.
+   *
+   * @param other The right hand side Matrix of the function being applied
+   * @param to_apply Binary function to apply elementwise to this and other,
+   * which accepts two Scalar values and returns a Scalar value
+   *
+   * @return New Matrix whose elements are the result of the function
+   * */
+  void elementwise_binary_apply_inplace(
+      Matrix<Scalar> const &other,
+      std::function<Scalar(Scalar, Scalar)> to_apply) {
+    if ((this->nrows != other.nrows) || (this->ncols != other.ncols)) {
+      throw std::runtime_error("Tried to add Matrices of different shapes");
+    }
+    for (size_t row = 0; row < this->nrows; row++) {
+      for (size_t col = 0; col < this->ncols; col++) {
+        *(*this)(row, col) = to_apply(*(*this)(row, col), *(*this)(row, col));
+      }
+    }
+  }
+  /*! Elementwise addition of two matrices*/
+  Matrix<Scalar> operator+(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs + lhs; });
+  }
+  /*! Elementwise addition of a Matrix and a Scalar element*/
+  Matrix<Scalar> operator+(Scalar other) {
+    return this->scalar_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs + lhs; });
+  }
+  /*! Elementwise inplace addition of two matrices*/
+  friend Matrix<Scalar> &operator+=(Matrix<Scalar> &lhs,
+                                    Matrix<Scalar> const &rhs) {
+    lhs.elementwise_binary_apply_inplace(
+        rhs, [](Scalar l, Scalar r) { return l + r; });
+    return lhs;
+  }
+  /*! Elementwise inplace addition of a Matrix and a Scalar */
+  friend Matrix<Scalar> &operator+=(Matrix<Scalar> &lhs, Scalar rhs) {
+    lhs.scalar_binary_apply_inplace(rhs,
+                                    [](Scalar l, Scalar r) { return l + r; });
+    return lhs;
+  }
+  /*! Elementwise subtraction of two matrices*/
+  Matrix<Scalar> operator-(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs - lhs; });
+  }
+  /*! Elementwise subtraction of a Matrix and a Scalar element*/
+  Matrix<Scalar> operator-(Scalar other) {
+    return this->scalar_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs - lhs; });
+  }
+  /*! Elementwise inplace subtraction of two matrices*/
+  friend Matrix<Scalar> &operator-=(Matrix<Scalar> &lhs,
+                                    Matrix<Scalar> const &rhs) {
+    lhs.elementwise_binary_apply_inplace(
+        rhs, [](Scalar l, Scalar r) { return l - r; });
+    return lhs;
+  }
+  /*! Elementwise inplace subtraction of a Matrix and a Scalar */
+  friend Matrix<Scalar> &operator-=(Matrix<Scalar> &lhs, Scalar rhs) {
+    lhs.scalar_binary_apply_inplace(rhs,
+                                    [](Scalar l, Scalar r) { return l - r; });
+    return lhs;
+  }
+  /*! Elementwise multiplication of two matrices*/
+  Matrix<Scalar> operator*(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs * lhs; });
+  }
+  /*! Elementwise multiplication of a Matrix and a Scalar element*/
+  Matrix<Scalar> operator*(Scalar other) {
+    return this->scalar_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs * lhs; });
+  }
+  /*! Elementwise inplace multiplication of two matrices*/
+  friend Matrix<Scalar> &operator*=(Matrix<Scalar> &lhs,
+                                    Matrix<Scalar> const &rhs) {
+    lhs.elementwise_binary_apply_inplace(
+        rhs, [](Scalar l, Scalar r) { return l * r; });
+    return lhs;
+  }
+  /*! Elementwise inplace multiplication of a Matrix and a Scalar */
+  friend Matrix<Scalar> &operator*=(Matrix<Scalar> &lhs, Scalar rhs) {
+    lhs.scalar_binary_apply_inplace(rhs,
+                                    [](Scalar l, Scalar r) { return l * r; });
+    return lhs;
+  }
+  /*! Elementwise division of two matrices*/
+  Matrix<Scalar> operator/(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs / lhs; });
+  }
+  /*! Elementwise division of a Matrix and a Scalar element*/
+  Matrix<Scalar> operator/(Scalar other) {
+    return this->scalar_binary_apply<Scalar>(
+        other, [](Scalar rhs, Scalar lhs) { return rhs / lhs; });
+  }
+  /*! Elementwise inplace division of two matrices*/
+  friend Matrix<Scalar> &operator/=(Matrix<Scalar> &lhs,
+                                    Matrix<Scalar> const &rhs) {
+    lhs.elementwise_binary_apply_inplace(
+        rhs, [](Scalar l, Scalar r) { return l / r; });
+    return lhs;
+  }
+  /*! Elementwise inplace division of a Matrix and a Scalar */
+  friend Matrix<Scalar> &operator/=(Matrix<Scalar> &lhs, Scalar rhs) {
+    lhs.scalar_binary_apply_inplace(rhs,
+                                    [](Scalar l, Scalar r) { return l / r; });
+    return lhs;
+  }
+  // SECTION: Boolean comparison overloads
+  /*! Elementwise equality comparison between this Matrix and other*/
+  Matrix<bool> operator==(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs == rhs; });
+  }
+  /*! Elementwise equality comparison between this Matrix and a Scalar other*/
+  Matrix<bool> operator==(Scalar other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs == rhs; });
+  }
+  /*! Elementwise less than comparison between this Matrix and other*/
+  Matrix<bool> operator<(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs < rhs; });
+  }
+  /*! Elementwise less than comparison between this Matrix and a Scalar other*/
+  Matrix<bool> operator<(Scalar other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs < rhs; });
+  }
+  /*! Elementwise less equal comparison between this Matrix and other*/
+  Matrix<bool> operator<=(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs <= rhs; });
+  }
+  /*! Elementwise less equal comparison between this Matrix and a Scalar other*/
+  Matrix<bool> operator<=(Scalar other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs <= rhs; });
+  }
+  /*! Elementwise greater than comparison between this Matrix and other*/
+  Matrix<bool> operator>(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs > rhs; });
+  }
+  /*! Elementwise greater than comparison between this Matrix and a Scalar
+   * other*/
+  Matrix<bool> operator>(Scalar other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs > rhs; });
+  }
+  /*! Elementwise greater equal comparison between this Matrix and other*/
+  Matrix<bool> operator>=(Matrix<Scalar> const &other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs >= rhs; });
+  }
+  /*! Elementwise greater equal comparison between this Matrix and a Scalar
+   * other*/
+  Matrix<bool> operator>=(Scalar other) {
+    return this->elementwise_binary_apply<bool>(
+        other, [](Scalar lhs, Scalar rhs) { return lhs >= rhs; });
+  }
+  // SECTION: Boolean helper functions
+  /*! Returns True if any of the elements of this Matrix are truthy*/
+  bool any() {
+    for (size_t row = 0; row < this->nrows; row++) {
+
+      for (size_t col = 0; col < this->ncols; col++) {
+        if (*(*this)(row, col)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  /*! Returns True if all of the elements of this Matrix are truthy*/
+  bool all() {
+    bool all_truthy = true;
+    for (size_t row = 0; row < this->nrows; row++) {
+      for (size_t col = 0; col < this->ncols; col++) {
+        if (!(*(*this)(row, col))) {
+          all_truthy = false;
+        }
+      }
+    }
+    return all_truthy;
   }
 }; // namespace template<typenameScalar>class TeensyMatrix
 } // namespace teensymat
--- tests/src/test_matrix.cpp
+++ tests/src/test_matrix.cpp
@@ -1,5 +1,6 @@
 // External Includes
 #include "catch2/catch_test_macros.hpp"
+#include "catch2/matchers/catch_matchers_floating_point.hpp"
 
 // Local Includes
 #include "TeensyOpt/TeensyMat/matrix_core.hpp"
@@ -55,4 +56,71 @@ TEST_CASE("Basic Matrix Operations", "[operations]") {
     REQUIRE(*test_matrix(2, 0) == 6);
     REQUIRE(*test_matrix(2, 1) == 5);
   }
+  SECTION("Multiply row by scalar") {
+    auto test_matrix = teensymat::Matrix<int>{2, 3, {1, 2, 3, 4, 5, 6}};
+    test_matrix.mult_row_scalar(0, 2);
+    std::vector<int> expected_row{2, 4, 6};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE(*test_matrix(0, i) == expected_row.at(i));
+    }
+  }
+  SECTION("Divide row by scalar") {
+    auto test_matrix = teensymat::Matrix<float>{2, 3, {1, 2, 3, 4, 5, 6}};
+    test_matrix.div_row_scalar(0, 2);
+    std::vector<float> expected_row{0.5, 1.0, 1.5};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE_THAT(*test_matrix(0, i),
+                   Catch::Matchers::WithinAbs(expected_row.at(i), 1e-12));
+    }
+  }
+  SECTION("Add scalar to row") {
+    auto test_matrix = teensymat::Matrix<int>{2, 3, {1, 2, 3, 4, 5, 6}};
+    test_matrix.add_row_scalar(0, 2);
+    std::vector<int> expected_row{3, 4, 5};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE(*test_matrix(0, i) == expected_row.at(i));
+    }
+  }
+  SECTION("Subtract scalar from row") {
+    auto test_matrix = teensymat::Matrix<int>{2, 3, {1, 2, 3, 4, 5, 6}};
+    test_matrix.sub_row_scalar(0, 1);
+    std::vector<int> expected_row{0, 1, 2};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE(*test_matrix(0, i) == expected_row.at(i));
+    }
+  }
+  SECTION("Multiply column by scalar") {
+    auto test_matrix = teensymat::Matrix<int>{3, 2, {1, 4, 2, 5, 3, 6}};
+    test_matrix.mult_col_scalar(0, 2);
+    std::vector<int> expected_col{2, 4, 6};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE(*test_matrix(i, 0) == expected_col.at(i));
+    }
+  }
+  SECTION("Divide column by scalar") {
+    auto test_matrix = teensymat::Matrix<float>{3, 2, {1, 4, 2, 5, 3, 6}};
+    test_matrix.div_col_scalar(0, 2);
+    std::vector<float> expected_col{0.5, 1.0, 1.5};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE_THAT(*test_matrix(i, 0),
+                   Catch::Matchers::WithinAbs(expected_col.at(i), 1e-12));
+    }
+  }
+  SECTION("Add scalar to column") {
+    auto test_matrix = teensymat::Matrix<int>{3, 2, {1, 4, 2, 5, 3, 6}};
+    test_matrix.add_col_scalar(0, 2);
+    std::vector<int> expected_col{3, 4, 5};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE(*test_matrix(i, 0) == expected_col.at(i));
+    }
+  }
+  SECTION("Subtract scalar from column") {
+    auto test_matrix = teensymat::Matrix<int>{3, 2, {1, 4, 2, 5, 3, 6}};
+    test_matrix.sub_col_scalar(0, 1);
+    std::vector<int> expected_col{0, 1, 2};
+    for (int i = 0; i < 3; i++) {
+      REQUIRE(*test_matrix(i, 0) == expected_col.at(i));
+    }
+  }
+  // SECTION("FAIL TEST") { REQUIRE(false); }
 }
